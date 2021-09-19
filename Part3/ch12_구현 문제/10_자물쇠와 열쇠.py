964e403336da8a59f1e64b519f87bdbab643dbb9
hyein99 <hyeinnn999@gmail.com>
2021-09-20 01:40:30 +0900

    0920

--- Part3/ch12_구현 문제/10_자물쇠와 열쇠.py
+++ Part3/ch12_구현 문제/10_자물쇠와 열쇠.py
@@ -5,26 +5,41 @@
 # 0: 홈, 1: 돌기
 
 def solution(key, lock):
+    M = len(key)
+    N = len(lock)
+
+    # 키를 90도 회전하는 함수
+    def rotate90(key):
+        return [[key[j][i] for j in range(M)] for i in range(M - 1, -1, -1)]
+
     # 키가 자물쇠에 부합하는지 판단하는 함수
-    def compare(key, lock):
-        for i in range(len(lock)):
-            for j in range(len(lock)):
-                if lock[i][j] == key[i][j]:
+    def compare(newlock):
+        for i in range(N, 2 * N):
+            for j in range(N, 2 * N):
+                if newlock[i][j] != 1:
                     return False
         return True
 
-    # 키를 회전하여 자물쇠와 비교
-    rotated = []
-    rotated.append([[key[j][i] for i in range(len(key))] for j in range(len(key))])
-    rotated.append([[key[i][j] for i in range(len(key) - 1, -1, -1)] for j in range(len(key))])
-    rotated.append([[key[i][j] for i in range(len(key) - 1, -1, -1)] for j in range(len(key) - 1, -1, -1)])
-    rotated.append([[key[i][j] for i in range(len(key))] for j in range(len(key) - 1, -1, -1)])
+    # step 1) 자물쇠 크기를 3배하기(상하좌우이동)
+    newlock = [[0 for _ in range(3 * N)] for _ in range(3 * N)]
+    for i in range(N):
+        for j in range(N):
+            newlock[N + i][N + j] = lock[i][j]
 
-    print(rotated)
-
-    # 90도 회전
-    # 좌우 이동
-    # 상하 이동
-
-    answer = True
-    return answer
+    # step 2) 4가지 방향으로 회전
+    for _ in range(4):
+        key = rotate90(key)
+        for x in range(N * 2):
+            for y in range(N * 2):
+                # 자물쇠(newlock)에 열쇠(key) 넣기
+                for i in range(M):
+                    for j in range(M):
+                        newlock[x + i][y + j] += key[i][j]
+                # 열쇠가 맞는지 확인
+                if compare(newlock):
+                    return True
+                # 자물쇠에서 열쇠 다시 빼기
+                for i in range(M):
+                    for j in range(M):
+                        newlock[x + i][y + j] -= key[i][j]
+    return False
