0ff6c1974b7ea526cdfbc3f3593a8b2904013a55
hyein99 <hyeinnn999@gmail.com>
2022-01-14 00:10:07 +0900

    0114

--- Part2/ch05_DFS BFS/04_미로 탈출.py
+++ Part2/ch05_DFS BFS/04_미로 탈출.py
@@ -1,3 +1,7 @@
+# 미로 탈출
+# BFS를 이용했을 때 효과적
+# BFS는 시작 지점에서 가까운 노드부터 차례대로 그래프의 모든 노드를 탐색하기 때문
+
 from collections import deque
 
 def bfs():
@@ -11,8 +15,8 @@ def bfs():
 
         for d in dir:
             nx, ny = x+d[0], y+d[1]
-            if 0<=nx<N and 0<=ny<M and maze[nx][ny] == '1': # 이동할 수 있는 위치
-                maze[nx][ny] = '0'
+            if 0<=nx<N and 0<=ny<M and maze[nx][ny] == '1':  # 이동할 수 있는 위치
+                maze[nx][ny] = '0'  # 방문표시
                 dist[nx][ny] = dist[x][y]+1
                 qu.append((nx, ny))
 
@@ -21,7 +25,7 @@ def bfs():
 N, M = map(int, input().split())
 maze = []
 for _ in range(N):
-    maze.append(list(input()))
+    maze.append(list(input()))  # 0: 괴물, 1: 길
 
 dir = [(1,0), (-1,0), (0, 1), (0, -1)]
 dist = [[1]*M for _ in range(N)]
